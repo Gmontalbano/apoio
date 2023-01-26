57adb4a07a2803b5870516f7fbb754f15f864617
Guilherme <gmontalbano38@gmail.com>
2023-01-25 22:43:17 -0300

    change to json

--- controle.py
+++ controle.py
@@ -1,44 +1,59 @@
 import streamlit as st
 import pandas as pd
+import json
+
+from pandas.io.json import json_normalize
 
 
 def add_item():
-    name = st.text_input("Nome do item")
-    quantity = st.number_input("Quantidade", step=1, min_value=0)
-    if st.button('Adicionar item'):
-        # Carrega o arquivo xlsx com os dados do estoque
-        stock = pd.read_excel("stock.xlsx")
-
-        # Adiciona o novo item ao estoque
-        new_item = {"name": name, "quantity": quantity, "solicitado": 0}
-        stock = stock.append(new_item, ignore_index=True)
-
-        # Salva as alterações no arquivo xlsx
-        stock.to_excel("stock.xlsx", index=False)
+    # Carrega o arquivo JSON com os dados do estoque
+    with open("stock.json", "r") as file:
+        stock = json.load(file)
+    # Recebe o nome do item a ser adicionado
+    item_name = st.text_input("Insira o nome do item a ser adicionado")
+    # Recebe a quantidade do item a ser adicionado
+    item_quantity = st.number_input("Insira a quantidade do item a ser adicionado", min_value=1, value=1)
+    if st.button("Adicionar item"):
+        # Adiciona o novo item ao dicionário
+        #  stock.append = {"name": item_name, 'quantity': item_quantity}
+        # Salva as alterações no arquivo JSON
+        with open("stock.json", "r+") as file:
+            stock = json.load(file)
+            stock["items"].append({"name": item_name, 'quantity': item_quantity})
+            file.seek(0)
+            json.dump(stock, file)
         st.success("Item adicionado com sucesso!")
 
 
 def delete_item():
-    # Carrega o arquivo xlsx com os dados do estoque
-    stock = pd.read_excel("stock.xlsx")
+    # Carrega o arquivo JSON com os dados do estoque
+    with open("stock.json", "r") as file:
+        stock = json.load(file)
     # Cria um menu dropdown com os nomes dos itens no estoque
-    item_name = st.selectbox("Selecione o item para deletar", stock["name"])
+    item_name = st.selectbox("Selecione o item para deletar", [item["name"] for item in stock["items"]])
     if st.button("Deletar item"):
-        # Encontra a linha correspondente ao item selecionado
-        item_to_delete = stock[stock['name'] == item_name]
-        # Deleta a linha do DataFrame
-        stock = stock.drop(item_to_delete.index)
-        # Salva as alterações no arquivo xlsx
-        stock.to_excel("stock.xlsx", index=False)
+        # Deleta o item selecionado do estoque
+        for i, item in enumerate(stock["items"]):
+            if item["name"] == item_name:
+                del stock["items"][i]
+                break
+        # Salva as alterações no arquivo JSON
+        with open("stock.json", "w") as file:
+            json.dump(stock, file)
         st.success("Item deletado com sucesso!")
 
 
 def view_stock():
-    # Carrega o arquivo xlsx com os dados do estoque
-    stock = pd.read_excel('stock.xlsx')
-    # Cria uma tabela para exibir os itens do estoque
-    st.write("Itens do estoque:")
-    st.table(stock)
+    # Carrega o arquivo JSON com os dados do estoque
+    with open("stock.json", "r") as file:
+        stock = json.load(file)
+
+    # Converte o dicionário para um dataframe
+    # df = pd.DataFrame(stock["items"])
+    df = json_normalize(stock["items"])
+
+    # Exibe a tabela do estoque
+    st.dataframe(df)
 
 
 def main_controle():
--- sol.py
+++ sol.py
@@ -1,35 +1,44 @@
 import streamlit as st
 import pandas as pd
+import json
 
 
 def solicitar_item():
-    # Carrega o arquivo xlsx com os dados do estoque
-    stock = pd.read_excel("stock.xlsx")
+    # Carrega o arquivo JSON com os dados do estoque
+    with open("stock.json", "r") as file:
+        stock = json.load(file)
     # Cria um menu dropdown com os nomes dos itens no estoque
-    item_name = st.selectbox("Selecione o item para solicitar", stock["name"])
-    # Pega a quantidade disponível do item selecionado
-    item_quantity = stock.loc[stock["name"] == item_name, "quantity"].values[0]
-    al_sol = stock.loc[stock["name"] == item_name, "solicitado"].values[0]
-    # Pede ao usuário para inserir a quantidade desejada
-    requested_quantity = st.number_input("Insira a quantidade desejada (máximo {})".format(item_quantity+al_sol), min_value=1, max_value=item_quantity)
+    item_name = st.selectbox("Selecione o item para solicitar", [item["name"] for item in stock["items"]])
+    # Recebe a quantidade do item solicitado
+    item_quantity = st.number_input("Insira a quantidade do item solicitado", min_value=1, value=1)
     if st.button("Solicitar item"):
-        # Atualiza a quantidade do item no estoque
-        stock.loc[stock["name"] == item_name, "solicitado"] -= requested_quantity
-        stock.to_excel("stock.xlsx", index=False)
+        # Atualiza a quantidade do item solicitado
+        for item in stock["items"]:
+            if item["name"] == item_name:
+                item["quantity"] -= item_quantity
+                break
+        # Salva as alterações no arquivo JSON
+        with open("stock.json", "w") as file:
+            json.dump(stock, file)
         st.success("Item solicitado com sucesso!")
 
 
 def devolver_item():
-    # Carrega o arquivo xlsx com os dados do estoque
-    stock = pd.read_excel("stock.xlsx")
+    # Carrega o arquivo JSON com os dados do estoque
+    with open("stock.json", "r") as file:
+        stock = json.load(file)
     # Cria um menu dropdown com os nomes dos itens no estoque
-    item_name = st.selectbox("Selecione o item para devolver", stock["name"])
-    al_sol = stock.loc[stock["name"] == item_name, "solicitado"].values[0]
-    # Pede ao usuário para inserir a quantidade a ser devolvida
-    returned_quantity = st.number_input("Insira a quantidade a ser devolvida", min_value=1, max_value=al_sol*-1)
+    item_name = st.selectbox("Selecione o item para devolver", [item["name"] for item in stock["items"]])
+    # Recebe a quantidade do item a ser devolvido
+    item_quantity = st.number_input("Insira a quantidade do item a ser devolvido", min_value=1, value=1)
     if st.button("Devolver item"):
-        # Atualiza a quantidade do item no estoque
-        stock.loc[stock["name"] == item_name, "solicitado"] += returned_quantity
-        stock.to_excel("stock.xlsx", index=False)
+        # Atualiza a quantidade do item devolvido
+        for item in stock["items"]:
+            if item["name"] == item_name:
+                item["quantity"] += item_quantity
+                break
+        # Salva as alterações no arquivo JSON
+        with open("stock.json", "w") as file:
+            json.dump(stock, file)
         st.success("Item devolvido com sucesso!")
 
