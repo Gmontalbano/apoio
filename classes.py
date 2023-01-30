c8c7bdaba3e9be4c8de231e22012ace7b05061c0
Guilherme <gmontalbano38@gmail.com>
2023-01-30 20:26:44 -0300

    CATEGORIA TO CLASSE

--- classes.py
+++ classes.py
@@ -18,17 +18,17 @@ def make_class():
     if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
         dbv = []
-        df['CATEGORIA'] = 'AMIGO'
+        df['CLASSE'] = 'AMIGO'
         df.loc[(df['DATA DE NASCIMENTO'] >= '2011-06-30') & (
-                    df['DATA DE NASCIMENTO'] <= '2012-06-30'), 'CATEGORIA'] = 'COMPANHEIRO'
+                    df['DATA DE NASCIMENTO'] <= '2012-06-30'), 'CLASSE'] = 'COMPANHEIRO'
         df.loc[(df['DATA DE NASCIMENTO'] >= '2010-06-30') & (
-                    df['DATA DE NASCIMENTO'] <= '2011-06-30'), 'CATEGORIA'] = 'PESQUISADOR'
+                    df['DATA DE NASCIMENTO'] <= '2011-06-30'), 'CLASSE'] = 'PESQUISADOR'
         df.loc[(df['DATA DE NASCIMENTO'] >= '2009-06-30') & (
-                    df['DATA DE NASCIMENTO'] <= '2010-06-30'), 'CATEGORIA'] = 'PIONEIRO'
+                    df['DATA DE NASCIMENTO'] <= '2010-06-30'), 'CLASSE'] = 'PIONEIRO'
         df.loc[(df['DATA DE NASCIMENTO'] >= '2008-06-30') & (
-                    df['DATA DE NASCIMENTO'] <= '2009-06-30'), 'CATEGORIA'] = 'EXCURSIONISTA'
+                    df['DATA DE NASCIMENTO'] <= '2009-06-30'), 'CLASSE'] = 'EXCURSIONISTA'
         df.loc[(df['DATA DE NASCIMENTO'] >= '2007-06-30') & (
-                    df['DATA DE NASCIMENTO'] <= '2008-06-30'), 'CATEGORIA'] = 'GUIA'
+                    df['DATA DE NASCIMENTO'] <= '2008-06-30'), 'CLASSE'] = 'GUIA'
         #new_df = pd.DataFrame(dbv)
         dff = to_excel(df)
         st.download_button("Download", dff, file_name='classes.xlsx')
