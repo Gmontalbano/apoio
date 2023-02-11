ca9915cf2eef252edb38af46ce0a96916ab117fc
Guilherme <gmontalbano38@gmail.com>
2023-02-10 22:33:02 -0300

    Calendario from db

--- cal.py
+++ cal.py
@@ -33,25 +33,51 @@ def tt_cal():
 
 
 def cal_2():
-
     year = date.today().year
     month = date.today().month
     # Criar tabela para eventos
     # Chamar a tabela e pegar os eventos do mes atual
     dates = [{'Dia': 7, 'Evento': 'niver'}, {'Dia': 11, 'Evento': 'teste'}]
     text_cal = calendar.HTMLCalendar(firstweekday=0)
-
-    x = text_cal.formatmonth(year, month).replace("Mon", "Seg").replace("Tue", "Ter").replace("Wed", "Qua")\
-        .replace("Thu", "Qui").replace("Fri", "Sex").replace("Sat", "Sab").replace("Sun", "Dom")\
+    meses = {'January': 'Janeiro',
+             'February': 'Fevereiro',
+             'March': 'Março',
+             'April': 'Abril',
+             'May': 'Maio',
+             'June': 'Junho',
+             'July': 'Julho',
+             'August': 'Agosto',
+             'September': 'Setembro',
+             'October': 'Outubro',
+             'November': 'Novembro',
+             'December': 'Dezembro'}
+    M_ING = date.today().strftime("%B")
+    x = text_cal.formatmonth(year, month).replace(F"{M_ING}", meses[f'{M_ING}']).replace("Mon", "Seg").replace("Tue", "Ter").replace("Wed", "Qua") \
+        .replace("Thu", "Qui").replace("Fri", "Sex").replace("Sat", "Sab").replace("Sun", "Dom") \
         .replace('border="0"', 'border="2"').replace('cellpadding="0"', 'cellpadding="2"')
-    for event in dates:
-        x = x.replace(f">{event['Dia']}<", f'style="background-color:#464e5f">{event["Dia"]}<')
+    import sqlite3
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    query = f"SELECT * FROM calendario WHERE mes = {month} AND ano = {year}"
+    df = pd.read_sql(query, conn)
+    df = df.reset_index()
+    co, ev = st.columns(2)
+    f = len(df) / 2
+    i = 1
+    itable = f'<body><table border="2" cellpadding="2" cellspacing="0" class="month">' \
+             f'<tr><th class="dia">Dia</th><th class="ev">Evento</th>'
+    padrao = '<tr><td class="dia">{data}</td><td class="ev">{texto}</td></tr>'
+    fim = '</table></body>'
+    for index, row in df.iterrows():
+        x = x.replace(f">{row['dia']}<", f'style="background-color:#464e5f">{row["dia"]}<')
+        n = padrao.replace('{data}', f"{row['dia']}").replace('{texto}', f"{row['evento']}")
+        itable += n
+    itable += fim
 
     x = x.split("<body>")
     x = x[0]
 
-    co, ev = st.columns(2)
     with co:
         stc.html(x, height=250, width=250)
-    df = pd.DataFrame.from_dict(dates)
-    ev.dataframe(df)
+    with ev:
+        stc.html(itable, height=250, width=250)
